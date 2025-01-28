6fb98eaeaaed993dc1a6106fa575365541e705ee
Panagiotis Prattis <panagiotispr1997@gmail.com>
2025-01-28 23:59:23 +0200

    tasks component and integration with Input decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,7 +10,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'users/' + this.avatar;
