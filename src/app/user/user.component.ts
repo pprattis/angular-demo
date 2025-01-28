1cf07802d5f95792d6900f78837f31dc55eb45f1
Panagiotis Prattis <panagiotispr1997@gmail.com>
2025-01-28 23:46:20 +0200

    integrate output function to emit data and event

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input, output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,7 +10,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return 'users/' + this.avatar;
