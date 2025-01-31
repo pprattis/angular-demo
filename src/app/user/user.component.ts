840e90f6b2f4ac1180122f274dd16b2b61071774
Panagiotis Prattis <panagiotispr1997@gmail.com>
2025-01-31 23:19:38 +0200

    optional types integration

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,9 +7,9 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) id?: string;
+  @Input({required: true}) avatar?: string;
+  @Input({required: true}) name?: string;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
